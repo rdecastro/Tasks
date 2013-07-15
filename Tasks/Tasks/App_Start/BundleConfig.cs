a018c04757ccf63b089c076d2f0f526491512d6e
rdecastro <zengineer@me.com>
2013-07-15 06:36:55 -0700

    - Added bundling and minification package - Added bundle configs for page styles and scripts

---
+++ Tasks/Tasks/App_Start/BundleConfig.cs
@@ -0,0 +1,23 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace Tasks
+{
+    public class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Content/tasks-scripts").Include(
+                    "~/Scripts/jquery-{version}.min.js", 
+                    "~/Scripts/bootstrap.min.js", 
+                    "~/Scripts/Tasks.js"));
+
+            bundles.Add(new StyleBundle("~/Content/tasks-styles").Include(
+                    "~/Content/bootstrap.min.css", 
+                    "~/Content/bootstrap-responsive.min.css"));
+
+        }
+    }
+
+}
